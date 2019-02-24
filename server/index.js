9064c450d699588ada8bb0a99ae5a33ea90d9e84
Chris Auret <chris.auret@yahoo.com>
2019-02-25 10:35:03 +1100

    cookie authentication with passport

--- server/index.js
+++ server/index.js
@@ -1,11 +1,21 @@
 const express = require('express');
 const mongoose = require('mongoose');
+const cookieSession = require('cookie-session');
+const passport = require('passport');
+
 const keys = require('./config/keys');
 require('./models/User');
 require('./services/passport');
 
 mongoose.connect(keys.mongoURI);
 const app = express();
+app.use(cookieSession({
+    maxAge: 30 * 24 *60 * 60 * 1000,
+    keys: [keys.cookieKey]
+}));
+
+app.use(passport.initialize());
+app.use(passport.session());
 
 // authRoutes returns function from that file (module.exports)
 // so we immidietly invoke that function  with app
